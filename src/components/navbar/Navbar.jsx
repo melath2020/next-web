ce1927b075353815a615b966faee29588aa23d1e
Aswin s <aswinsudhakaran98@gmail.com>
2023-06-14 05:41:21 +0530

    update

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Link href='/'>lamalamia</Link>
+    </div>
+  )
+}
+
+export default Navbar
